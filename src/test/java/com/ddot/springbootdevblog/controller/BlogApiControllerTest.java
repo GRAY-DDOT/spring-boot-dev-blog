419854cb1200d1b6bd2a6560aa097741ecab0ece
GRAY-DDOT <hyeonhc328@gmail.com>
2024-08-16 18:47:37 +0900

    TEST : Add tests for blog post deletion API
    - Add tests.
    - Also removed unused import statements in the test file.

--- src/test/java/com/ddot/springbootdevblog/controller/BlogApiControllerTest.java
+++ src/test/java/com/ddot/springbootdevblog/controller/BlogApiControllerTest.java
@@ -13,12 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
@@ -27,7 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -49,8 +46,7 @@ class BlogApiControllerTest {
 
     @Autowired
     BlogRepository blogRepository;
-    @Autowired
-    private BlogService blogService;
+
 
     @BeforeEach // 테스트 실행 전 실행
     public void mockMVCSetUp() {
