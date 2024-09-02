3cf8178ccf41f9f45a389f0e0f6d0fddf989d5e7
GRAY-DDOT <hyeonhc328@gmail.com>
2024-09-02 19:41:01 +0900

    FEAT : Add Logout Service and Views
    Signed-off-by: GRAY-DDOT <hyeonhc328@gmail.com>

--- src/main/java/com/ddot/springbootdevblog/controller/UserApiController.java
+++ src/main/java/com/ddot/springbootdevblog/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.ddot.springbootdevblog.controller;
 
 import com.ddot.springbootdevblog.dto.AddUserRequest;
 import com.ddot.springbootdevblog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +20,13 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 
